518e3498a840e3e54213f7186279dc6ed1585886
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-07 09:45:21 -0600

    Add eks cluster info & auth datasource

---
+++ data.tf
@@ -0,0 +1,9 @@
+# Retrive information about the EKS Cluster
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+# Get an authentication token to communicate with an EKS cluster
+data "aws_eks_cluster_auth" "this" { 
+  name = var.cluster_name 
+} 
