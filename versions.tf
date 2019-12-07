1bb10b8c5941751d3d0f87216c7cf882cc2d3eb6
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-07 14:29:17 -0600

    add terraform, aws, helm, k8s version and config

---
+++ versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.12.16"
+
+  required_providers {
+    aws       = ">= 2.41.0"
+    terraform = ">= 1.10.0"
+    helm      = ">= 0.10.4"
+  }
+}
+ 
+provider "kubernetes" { 
+  host                   = data.aws_eks_cluster.this.endpoint
+  token                  = data.aws_eks_cluster_auth.this.token 
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+  load_config_file       = false
+} 
+ 
+provider "helm" { 
+  install_tiller  = true 
+  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.16.1" 
+  service_account = "tiller"
+
+  kubernetes { 
+    host                   = data.aws_eks_cluster.this.endpoint
+    token                  = data.aws_eks_cluster_auth.this.token 
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority.0.data)
+    load_config_file       = false 
+  } 
+} 
