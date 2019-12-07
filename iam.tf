b0b457d70156d7f310453ba1d0988ac9e1fbf825
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-07 10:19:19 -0600

    Add iam oidc provider association

---
+++ iam.tf
@@ -0,0 +1,6 @@
+# Associate IAM oidc provider 
+resource "aws_iam_openid_connect_provider" "this" { 
+  client_id_list  = ["sts.amazonaws.com"] 
+  thumbprint_list = [] 
+  url             = data.aws_eks_cluster.example.identity.0.oidc.0.issuer
+}
