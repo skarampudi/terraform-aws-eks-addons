42abd0a3be46db59b5aefe199b8753e9e9bccfa2
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-08 11:54:37 -0600

    remove unwanted

--- variables.tf
+++ variables.tf
@@ -2,18 +2,3 @@ variable "cluster_name" {
   description = "The name of the EKS cluster."
   type        = string
 }
-
-variable "cluster_endpoint" {
-  description = "The endpoint for your EKS Kubernetes API."
-  type        = string
-}
-
-variable "cluster_certificate_authority_data" {
-  description = "PEM-encoded root certificates bundle for TLS authentication."
-  type        = string
-}
-
-variable "cluster_oidc_issuer_url" {
-  description = "The name of the EKS cluster."
-  type        = string
-}
