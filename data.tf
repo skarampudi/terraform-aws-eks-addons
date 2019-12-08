191331e0099b454fd4066af23c8f0aa030809361
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-08 00:53:50 -0600

    Remove cluster info lookup

--- data.tf
+++ data.tf
@@ -1,9 +1,4 @@
-# Retrive information about the EKS Cluster
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name
-}
-
 # Get an authentication token to communicate with an EKS cluster
 data "aws_eks_cluster_auth" "this" { 
-  name = var.cluster_name 
+  name = var.cluster_name
 } 
