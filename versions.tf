447b689ab27f7ef25f0bbc9eab94fb588a4fecc4
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-08 09:42:28 -0600

    remove helm provider

--- versions.tf
+++ versions.tf
@@ -2,16 +2,3 @@ terraform {
   required_version = ">= 0.12.6"
 
 }
- 
-provider "helm" { 
-  install_tiller  = true 
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.16.1" 
-  service_account = "tiller"
-
-  kubernetes { 
-    host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token 
-    load_config_file       = false 
-  } 
-} 
