75d4c2a0a41870122bd7712ae888a38462cb682f
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-08 13:55:08 -0600

    Add fluentd-cloudwatch helm chart

--- main.tf
+++ main.tf
@@ -11,3 +11,30 @@ resource "kubernetes_namespace" "amazon" {
  
   } 
 }
+
+# Install fluentd-cloudwatch 
+resource "helm_release" "fluentd-cloudwatch" { 
+  depends_on = [kubernetes_cluster_role_binding.tiller, kubernetes_namespace.amazon] 
+ 
+  name       = "fluentd-cloudwatch" 
+  chart      = "fluentd-cloudwatch" 
+  repository = data.helm_repository.incubator.metadata.0.name 
+  namespace  = "amazon" 
+  version    = "0.11.1" 
+ 
+  set { 
+    name  = "awsRegion" 
+    value = data.aws_region.current.name 
+  } 
+ 
+  set { 
+    name  = "logGroupName" 
+    value = "/aws/eks/${var.cluster_name}/application" 
+  } 
+ 
+  set { 
+    name  = "rbac.create" 
+    value = true 
+  } 
+ 
+}
