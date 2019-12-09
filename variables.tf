adf113fdeb6f1703f71890aa031cdb1442895e2f
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-08 23:50:55 -0600

    Add vpc id var

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cluster_name" {
   description = "The name of the EKS cluster."
   type        = string
 }
+
+variable "vpc_id" {
+  description = "VPC where the cluster and workers will be deployed."
+  type        = string
+}
