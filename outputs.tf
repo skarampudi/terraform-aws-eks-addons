c4f06d65118598ec8a221c2b6468974c8a155e3f
Sriharsha Karampudi <sriharsha@karampudi.me>
2019-12-08 23:53:16 -0600

    Add metedata outputs for aws-alb-ingress-controller and external-dns

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "fluentd_cloudwatch_helm_release_metadata" {
   description = "Block status of the deployed fluentd cloudwatch helm release."
   value       = helm_release.fluentd-cloudwatch.metadata
 }
+
+output "aws_alb_ingress_controller_helm_release_metadata" {
+  description = "Block status of the deployed aws-alb-ingress-controller helm release."
+  value       = helm_release.aws-alb-ingress-controller.metadata
+}
+
+output "external_dns_helm_release_metadata" {
+  description = "Block status of the deployed external-dns helm release."
+  value       = helm_release.external-dns.metadata
+}
